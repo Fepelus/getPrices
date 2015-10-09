fbfc9e978363aaefd674e44d0d3fd598322da672
Patrick Borgeest <spogboy@gmail.com>
2015-10-09 18:58:38 +1100

    Output legal ledger format

--- outputter/ledgerOutput.go
+++ outputter/ledgerOutput.go
@@ -1,37 +1,36 @@
 package outputter
 
 import (
-  "fmt"
-  "os"
-  
-  "github.com/Fepelus/getPrices/entities"
+	"fmt"
+	"os"
+
+	"github.com/Fepelus/getPrices/entities"
 )
 
 type ledgerOutputter struct {
-  channel chan entities.Price
-  count int
+	channel chan entities.Price
+	count   int
 }
 
 func NewLedgerOutputter(count int) Outputter {
-  return ledgerOutputter{
-    channel: make(chan entities.Price),
-    count: count,
-  }
+	return ledgerOutputter{
+		channel: make(chan entities.Price),
+		count:   count,
+	}
 }
 
 func (this ledgerOutputter) Append(input entities.Price) {
-   this.channel <- input 
+	this.channel <- input
 }
 
 func (this ledgerOutputter) Output() {
-  for i := 0; i < this.count; i++ {
-    this.formatPriceForLedger(<-this.channel)
-  }
+	for i := 0; i < this.count; i++ {
+		this.formatPriceForLedger(<-this.channel)
+	}
 }
 
 func (this ledgerOutputter) formatPriceForLedger(input entities.Price) {
 	dtfmt := input.Date.Format("2006/02/01")
 	clfmt := input.Clock.Format("15:04")
-	fmt.Fprintf(os.Stdout, "P %s %s %s $%s\n", dtfmt, clfmt, input.Ticker, input.Price)
+	fmt.Fprintf(os.Stdout, "P %s %s:00 %s $%s\n", dtfmt, clfmt, input.Ticker, input.Price)
 }
-
